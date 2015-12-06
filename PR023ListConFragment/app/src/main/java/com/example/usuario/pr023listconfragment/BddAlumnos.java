8621898bac2ebcf0823cff5371d3c9235ffcf7b2
Alejandro Torres <alejandro123888@hotmail.com>
2015-12-06 12:55:13 +0100

    DLC BDD ficticia2

---
+++ PR023ListConFragment/app/src/main/java/com/example/usuario/pr023listconfragment/BddAlumnos.java
@@ -0,0 +1,8 @@
+package com.example.usuario.pr023listconfragment;
+
+
+import java.util.ArrayList;
+//BDD ficticia.
+public class BddAlumnos {
+    public static ArrayList<Alumno> listaAlumnos = new ArrayList<>();
+}
