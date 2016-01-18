5d0811d2070e6f002c730be82946cfc3a9367d45
Alejandro Torres <alejandro123888@hotmail.com>
2016-01-18 23:23:11 +0100

    ejercicio php saladillo completed

--- PR028Reloj/app/src/main/java/com/example/usuario/pr028reloj/MainActivity.java
+++ PR028Reloj/app/src/main/java/com/example/usuario/pr028reloj/MainActivity.java
@@ -9,14 +9,15 @@ import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnIniciar;
     private TextView lblTiempo;
     private Thread hiloSecundario;
-
+    private final SimpleDateFormat formateador = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,17 +28,22 @@ public class MainActivity extends AppCompatActivity {
     private void initViews() {
         btnIniciar = (Button) findViewById(R.id.btnIniciar);
         lblTiempo = (TextView) findViewById(R.id.lblTiempo);
-        btnIniciar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (lblTiempo.getText().toString().equals(getResources().getString(R.string.iniciar)))
+        lblTiempo.setText(formateador.format(new Date()));
+        btnIniciar.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btnIniciar:
+                if (btnIniciar.getText().toString().equals(getResources().getString(R.string.iniciar)))
                     iniciar();
                 else
                     parar();
-            }
-        });
+                break;
+        }
     }
-
     private void iniciar(){
         hiloSecundario = new Thread(new Reloj(), "Secundario");
 
@@ -45,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
 
         btnIniciar.setText("Parar");
     }
+
     private void parar(){
         hiloSecundario.interrupt();
         btnIniciar.setText(getString(R.string.iniciar));
@@ -52,7 +59,9 @@ public class MainActivity extends AppCompatActivity {
 
     private class Reloj implements Runnable{
 
+
         final SimpleDateFormat formateador = new SimpleDateFormat("HH:mm:ss");
+
 
         @Override
         public void run() {
@@ -60,12 +69,19 @@ public class MainActivity extends AppCompatActivity {
                 lblTiempo.post(new Runnable() {
                     @Override
                     public void run() {
-                        lblTiempo.setText(formateador.format(new Date()));
+                        // Se obtiene la representación en cadena de la hora actual.
+                        // La variable debe ser final para que se pueda enviar en el
+                        // Runnable que se envía al hilo principal.
+                        final String cadena = formateador.format(new Date());
+
+                        lblTiempo.setText(cadena);
                     }
                 });
+                //Espera 1 segundo tras cambiar la cadena de la hora
                 try {
-                    TimeUnit.SECONDS.sleep(1);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
+                    //Se finaliza el hilo si el hilo es interrumpido mientras duerme
                     return;
                 }
 
--- PR029BarrasProgeso/app/src/main/java/com/example/usuario/pr029barrasprogeso/MainActivity.java
+++ PR029BarrasProgeso/app/src/main/java/com/example/usuario/pr029barrasprogeso/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         prbBarra = (ProgressBar) findViewById(R.id.prbBarra);
         prbCirculo = (ProgressBar) findViewById(R.id.prbCirculo);
         lblMensaje = (TextView) findViewById(R.id.lblMensaje);
+        btnIniciar = (Button) findViewById(R.id.btnIniciar);
         btnIniciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -54,12 +55,24 @@ public class MainActivity extends AppCompatActivity {
     private void mostrarBarras(){
         prbBarra.setVisibility(View.VISIBLE);
         prbCirculo.setVisibility(View.VISIBLE);
-        lblMensaje.setText(getString(R.string.trabajando));
+        lblMensaje.setText(R.string.trabajando);
         lblMensaje.setVisibility(View.VISIBLE);
     }
     private void actualizarBarras(int progreso){
         lblMensaje.setText(getString(R.string.trabajando, progreso, 10));
         prbBarra.setProgress(progreso);
+    }
+    //Muestra el total de las tareas realizadas.
+    private void mostrarRealizadas(int tareasRealizadas){
+        lblMensaje.setText(getResources().getQuantityString(R.plurals.realizadas, tareasRealizadas, tareasRealizadas));
+
+    }
+    private void resetearVistas(){
+        prbBarra.setVisibility(View.INVISIBLE);
+        prbBarra.setProgress(0);
+        prbCirculo.setVisibility(View.INVISIBLE);
+        prbCirculo.setProgress(0);
+        btnIniciar.setEnabled(true);
     }
 
 
@@ -67,7 +80,36 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void run() {
+            Message msgProgreso;
 
+            //Crea y envia el mensaje de inicio al manejador
+            Message msgInicio = new Message();
+            msgInicio.what = ANTES_DE_EMPEZAR;
+            manejador.sendMessage(msgInicio);
+
+            //Se realizan 10 pasos
+            for(int i = 0; i<10; i++){
+                trabajar();
+
+                msgProgreso = new Message();
+                msgProgreso.what = EN_PROGRESO;
+                msgProgreso.arg1= i + 1;
+                manejador.sendMessage(msgProgreso);
+            }
+            //Crea y envía el mensaje de fin al manejador
+            Message msgFin = new Message();
+            msgFin.what = AL_TERMINAR;
+            msgFin.arg1=10;
+            manejador.sendMessage(msgFin);
+
+        }
+        //Simula que trabaja 1 segundo
+        private void trabajar(){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
     //Manejador
@@ -83,8 +125,22 @@ public class MainActivity extends AppCompatActivity {
             MainActivity activity = mActivityWeakReference.get();
             if(activity!=null){
                 switch(msg.what){
+                    //Mensaje de inicio del hilo secundario
                     case ANTES_DE_EMPEZAR:
-
+                        //Se hacen visibles las barras de progreso
+                        activity.mostrarBarras();
+                        break;
+                    //Mensaje de en progreso del hilo secundario
+                    case EN_PROGRESO:
+                        int progreso = msg.arg1;
+                        activity.actualizarBarras(progreso);
+                        break;
+                    //Mensaje de fin del hilo secundario.
+                    case AL_TERMINAR:
+                        //Se informa al usuario y se reinician las vistas.
+                        int tareas = msg.arg1;
+                        activity.mostrarRealizadas(tareas);
+                        activity.resetearVistas();
                         break;
                 }
 
