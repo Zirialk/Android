18cdaa90e0b8914498bc6ab20723527ef42dcdcd
Alejandro Torres <alejandro123888@hotmail.com>
2016-02-07 17:41:45 +0100

    RecyclerView Configurado

---
+++ PR047Practica2trimestre/app/src/main/java/es/example/alex/pr047practica2trimestre/AlumnoAdapter.java
@@ -0,0 +1,97 @@
+package es.example.alex.pr047practica2trimestre;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+
+public class AlumnoAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private final List<Alumno> mDatos;
+    private OnItemClickListener onItemClickListener;
+    private View emptyView;
+
+    // Interfaz que debe implementar el listener para cuando se haga click sobre un elemento.
+    public interface OnItemClickListener {
+        void onItemClick(View view, Alumno alumno, int position);
+    }
+
+    public AlumnoAdapter(List<Alumno> datos) {
+        mDatos = datos;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, final int viewType) {
+        //Se infla el item de alumno.
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_alumno, parent, false);
+        final RecyclerView.ViewHolder viewHolder = new AlumnoViewHolder(itemView);
+
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(onItemClickListener != null)
+                    onItemClickListener.onItemClick(v, mDatos.get(viewHolder.getAdapterPosition()), viewHolder.getAdapterPosition());
+            }
+        });
+        //Se retorna el contenedor con la vista del item de alumno en su interior.
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        ((AlumnoViewHolder)holder).onBind(mDatos.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDatos.size();
+    }
+
+    static class AlumnoViewHolder extends RecyclerView.ViewHolder{
+
+        private final ImageView imgAvatar;
+        private final TextView lblNombre;
+
+        public AlumnoViewHolder(View itemView) {
+            super(itemView);
+            imgAvatar = (ImageView) itemView.findViewById(R.id.imgAvatar);
+            lblNombre = (TextView) itemView.findViewById(R.id.lblNombre);
+        }
+        public void onBind(Alumno alumno){
+            lblNombre.setText(alumno.getNombre());
+            Picasso.with(itemView.getContext()).load(alumno.getFoto()).into(imgAvatar);
+        }
+    }
+    public void addItem(Alumno alumno){
+        notifyItemInserted(mDatos.size()-1);
+        mDatos.add(alumno);
+    }
+    public void removeItem(int posicion){
+        mDatos.remove(posicion);
+        notifyItemRemoved(posicion);
+        checkIfEmpty();
+    }
+
+    // Establece el listener a informar cuando se hace click sobre un elemento de la lista.
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.onItemClickListener = listener;
+    }
+
+    private void checkIfEmpty() {
+        if(emptyView != null)
+            emptyView.setVisibility(getItemCount() > 0 ? View.GONE : View.VISIBLE);
+    }
+    // Establece la empty view para la lista.
+    public void setEmptyView(View emptyView) {
+        this.emptyView = emptyView;
+        // Muestra la empty view si  la lista está vacía.
+        checkIfEmpty();
+    }
+
+}
--- PR047Practica2trimestre/app/src/main/java/es/example/alex/pr047practica2trimestre/MainActivity.java
+++ PR047Practica2trimestre/app/src/main/java/es/example/alex/pr047practica2trimestre/MainActivity.java
@@ -2,15 +2,45 @@ package es.example.alex.pr047practica2trimestre;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements AlumnoAdapter.OnItemClickListener {
+
+    private RecyclerView rvAlumnos;
+    private AlumnoAdapter mAdaptador;
+    private LinearLayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        initViews();
+    }
+
+    private void initViews() {
+        configRecyclerView();
+        
+    }
+
+    private void configRecyclerView() {
+        rvAlumnos = (RecyclerView) findViewById(R.id.rvAlumnos);
+        rvAlumnos.setHasFixedSize(true);
+        mAdaptador = new AlumnoAdapter(new ArrayList<Alumno>());
+        mAdaptador.setOnItemClickListener(this);
+        rvAlumnos.setAdapter(mAdaptador);
+        mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        rvAlumnos.setLayoutManager(mLayoutManager);
+        rvAlumnos.setItemAnimator(new DefaultItemAnimator());
     }
 
     @Override
@@ -33,5 +63,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //Click en un item de la lista.
+    @Override
+    public void onItemClick(View view, Alumno alumno, int position) {
+
     }
 }
